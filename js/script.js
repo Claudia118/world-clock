5f7b87dc77a4e480892c3d09862cdfb56b4e8f47
Claudia Ortiz <102268001+Claudia118@users.noreply.github.com>
2023-05-13 15:56:40 -0600

    Getting date and time for one city using moment.js and moment-timezone

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
+function updateTime() {
+  let albuquerqueElement = document.querySelector("#albuquerque");
+  let albuquerqueDateElem = albuquerqueElement.querySelector(".date");
+  let albuquerqueTimeElem = albuquerqueElement.querySelector(".time");
+  let albuquerqueTime = moment().tz("US/Mountain");
 
+  albuquerqueDateElem.innerHTML = moment().format("MMMM Do YYYY");
+  albuquerqueTimeElem.innerHTML = `${albuquerqueTime.format("h:mm:ss [<small>]A[</small>]")} `;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
