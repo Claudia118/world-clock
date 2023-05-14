9bbcbc400a5d0e954e471edbbec0f153a27d3b0a
Claudia Ortiz <102268001+Claudia118@users.noreply.github.com>
2023-05-13 21:08:31 -0600

    Added link to all cities

--- js/script.js
+++ js/script.js
@@ -24,7 +24,8 @@ function updateCity(event) {
             <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
           </div>
           <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format("A")}</small></div>
-  </div>`;
+  </div>
+  <a href="/">All cities</a>`;
 }
 
 updateTime();
